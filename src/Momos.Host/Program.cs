fe6eba411f0aa2da5915d487e69ddb634ef4199b
uj <caveman@iyulab.com>
2026-08-27 16:18:10 +0900

    feat: apply EF Core migrations for MomosDbContext on Host startup

--- src/Momos.Host/Program.cs
+++ src/Momos.Host/Program.cs
@@ -1,11 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using Momos.Host.Data;
 using Momos.Worker;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddMomosWorker(builder.Configuration);
+builder.Services.AddDbContext<MomosDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("MomosDb")
+        ?? throw new InvalidOperationException("ConnectionStrings:MomosDb is required.")));
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    scope.ServiceProvider.GetRequiredService<MomosDbContext>().Database.Migrate();
+}
 
 if (app.Environment.IsDevelopment())
 {
@@ -17,3 +27,5 @@ app.UseHttpsRedirection();
 app.MapGet("/health", () => Results.Ok(new { status = "ok" }));
 
 app.Run();
+
+public partial class Program;
